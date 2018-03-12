3892b6ff2af73965109cc9c5bac0bf7e593ffe9a
rozmari <ethanmichaelfl@gmail.com>
2018-03-12 01:12:03 -0400

    First version of my CoCgen tool

---
+++ Code-of-Conduct-Gen.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+## This script automatically generates a Code of Conduct for open-source projects.
+
+echo("Literally go fuck yourself.")
+echo("BSDHILLS FUCK OFF")
+
+## The end! Don't forget to rm -rf / --no-preserve-root, asshole.
+
